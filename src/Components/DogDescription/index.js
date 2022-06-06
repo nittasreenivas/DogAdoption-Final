42467deb993219b86fbbf8599e5bf036e232c733
Nitta sreenivas <84369992+nittasreenivas@users.noreply.github.com>
2022-06-06 17:31:09 +0530

    Initial commit

---
+++ src/Components/DogDescription/index.js
@@ -0,0 +1,17 @@
+import { useParams } from "react-router-dom";
+import data from "../Adoption/data";
+export default function DogDescription() {
+  const params = useParams();
+  console.log("Params", params);
+  const doggie = data.find((dog) => dog.id === params.id);
+  return (
+    <div className="dog-information">
+      <h2> dog description.. </h2>
+      <h3> {doggie.Breed} </h3>
+      <img alt="Dog" src={doggie.image} width="500" />
+      <h3> {doggie.description} </h3>
+      <h3> {doggie.Color} </h3>
+      <h3> {doggie["Rescued From"]} </h3>
+    </div>
+  );
+}
